ebf99427d3159021ebec1dbf5b1256216733636e
unknown <chloe.stevovitch@gmail.com>
2020-11-13 22:18:57 -0500

    wip

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require("express");
+const path = require('path');
 
 var app = express();
-app.use(express.static("./dist/bullet"));
+app.use(express.static(__dirname + "./dist/bullet"));
 app.get("/*", function (req, res) {
   res.sendFile("index.html", { root: "dist/bullet/" });
+  res.sendFile(path.join(__dirname+'/dist/bullet/index.html'));
+
 });
 app.listen(process.env.PORT || 8080);
